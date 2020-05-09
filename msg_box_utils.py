018fa931e153f5c939e5fe35902f3d96ff6316be
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-09 17:45:10 -0500

    fixed header

--- msg_box_utils.py
+++ msg_box_utils.py
@@ -55,63 +55,6 @@ BTN_NUM_NAME_D = {
                    7 : 'no'    
                  }
 
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
-
-util_submodule_l = ['exception_utils']  # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
-
-# temporarily remove any modules that could conflict with this file's local util_submodule imports
-og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
-for module_descrip in sys.modules.keys():  
-    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
-for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
-util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
-
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-
-from util_submodules.exception_utils import exception_utils as eu                                       ; util_submodule_import_check_count += 1
-
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-# https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox?redirectedfrom=MSDN
-# https://stackoverflow.com/questions/27257018/python-messagebox-with-icons-using-ctypes-and-windll
-
-
-
-import ctypes
-
-
-
-TYPE_NUM__OK                 = 0 
-TYPE_NUM__OK_CANCEL          = 1 
-TYPE_NUM__ABORT_RETRY_IGNORE = 2 
-TYPE_NUM__YES_NO_CANCEL      = 3 
-TYPE_NUM__YES_NO             = 4
-TYPE_NUM__RETRY_CANCEL       = 5 
-TYPE_NUM__CRITICAL_MSG_ICON  = 16
-
-TYPE_NUM__STOP_ICON          = 0x10
-TYPE_NUM__QUESTION_ICON      = 0x20
-TYPE_NUM__EXLAIM_ICON        = 0x30
-TYPE_NUM__INFO_ICON          = 0x30
-
-ICON_KEY_TYPE_NUM_D = {
-                        'stop'    : TYPE_NUM__STOP_ICON    ,
-                        'question': TYPE_NUM__QUESTION_ICON,
-                        'exclaim' : TYPE_NUM__EXLAIM_ICON  ,
-                        'info'    : TYPE_NUM__INFO_ICON     
-                      }
-BTN_NUM_NAME_D = {
-                   1 : 'ok'    , # also X for OK
-                   2 : 'cancel', # also X for anything with CANCEL btn
-                   3 : 'abort' ,
-                   4 : 'retry' ,
-                   5 : 'ignore',
-                   6 : 'yes'   ,
-                   7 : 'no'    
-                 }
-
 
 
 ''' Internal '''
