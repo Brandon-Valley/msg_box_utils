15ae318ea063a8a69419e2169b8eb1c91b0d92b8
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-14 11:33:22 -0500

    switched to rel import method

--- msg_box_utils.py
+++ msg_box_utils.py
@@ -1,28 +1,14 @@
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
-
-util_submodule_l = ['exception_utils']  # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
-
-# temporarily remove any modules that could conflict with this file's local util_submodule imports
-og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
-for module_descrip in sys.modules.keys():  
-    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
-for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
-util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
-
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-
-from util_submodules.exception_utils import exception_utils as eu                                       ; util_submodule_import_check_count += 1
-
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
 # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox?redirectedfrom=MSDN
 # https://stackoverflow.com/questions/27257018/python-messagebox-with-icons-using-ctypes-and-windll
 
 
-
 import ctypes
+
+
+if __name__ == "__main__": 
+    from   usms.exception_utils import exception_utils as eu 
+else:
+    from . usms.exception_utils import exception_utils as eu
 
 
 
@@ -33,12 +19,12 @@ TYPE_NUM__YES_NO_CANCEL      = 3
 TYPE_NUM__YES_NO             = 4
 TYPE_NUM__RETRY_CANCEL       = 5 
 TYPE_NUM__CRITICAL_MSG_ICON  = 16
-
+ 
 TYPE_NUM__STOP_ICON          = 0x10
 TYPE_NUM__QUESTION_ICON      = 0x20
 TYPE_NUM__EXLAIM_ICON        = 0x30
 TYPE_NUM__INFO_ICON          = 0x30
-
+ 
 ICON_KEY_TYPE_NUM_D = {
                         'stop'    : TYPE_NUM__STOP_ICON    ,
                         'question': TYPE_NUM__QUESTION_ICON,
@@ -54,9 +40,9 @@ BTN_NUM_NAME_D = {
                    6 : 'yes'   ,
                    7 : 'no'    
                  }
-
-
-
+ 
+ 
+ 
 ''' Internal '''
 def root_msg_box(type_num, title, msg, icon, output_define_d, app_id):
     eu.error_if_param_type_not_in_whitelist(msg, ['str'])
@@ -64,23 +50,23 @@ def root_msg_box(type_num, title, msg, icon, output_define_d, app_id):
     eu.error_if_param_key_not_in_whitelist(icon, [None] + list(ICON_KEY_TYPE_NUM_D.keys()))
     eu.error_if_param_type_not_in_whitelist(output_define_d, ['dict', 'NoneType'])
     eu.error_if_param_type_not_in_whitelist(app_id, ['str', 'NoneType'])
-    
-    
+     
+     
     # add icon if given
     if icon != None:
         type_num = type_num | ICON_KEY_TYPE_NUM_D[icon]
-    
+     
     # sets tool bar icon to match parent's if any
     if app_id != None:
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
-    
-    
+     
+     
     # create msg_box
     MessageBox = ctypes.windll.user32.MessageBoxW
     out_num = MessageBox(None, msg, title, type_num)
     out_str = BTN_NUM_NAME_D[out_num]
-
-    
+ 
+     
     # change return if output_define_d given
     if output_define_d == None:
         return out_str
@@ -89,9 +75,9 @@ def root_msg_box(type_num, title, msg, icon, output_define_d, app_id):
             return output_define_d[out_str]
         else:
             raise Exception('ERROR:  "' + out_str + '" returned by the msg box is not a key in given output_define_d: ' + output_define_d)
-        
-        
-
+         
+         
+ 
 ''' External '''        
 def msg_box__OK                 (title, msg, icon = None, output_define_d = None, app_id = None): return root_msg_box(TYPE_NUM__OK                 , title, msg, icon, output_define_d, app_id)        
 def msg_box__OK_CANCEL          (title, msg, icon = None, output_define_d = None, app_id = None): return root_msg_box(TYPE_NUM__OK_CANCEL          , title, msg, icon, output_define_d, app_id)        
@@ -100,35 +86,33 @@ def msg_box__YES_NO_CANCEL      (title, msg, icon = None, output_define_d = None
 def msg_box__YES_NO             (title, msg, icon = None, output_define_d = None, app_id = None): return root_msg_box(TYPE_NUM__YES_NO             , title, msg, icon, output_define_d, app_id)        
 def msg_box__RETRY_CANCEL       (title, msg, icon = None, output_define_d = None, app_id = None): return root_msg_box(TYPE_NUM__RETRY_CANCEL       , title, msg, icon, output_define_d, app_id)        
 def msg_box__CRITICAL_MSG_ICON  (title, msg, icon = None, output_define_d = None, app_id = None): return root_msg_box(TYPE_NUM__CRITICAL_MSG_ICON  , title, msg, icon, output_define_d, app_id)        
-     
-
-        
-        
-        
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Footer -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-sys.modules = og_sys_modules
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+      
+ 
+         
+         
+         
+ 
 if __name__ == '__main__':
     print('In Main:  msg_box_utils')
+     
     
-   
-    
-    
-    
+     
+     
+     
     title = 'test title'
     msg = 'test msg'
     output_define_d = {'yes': True,
                        'no' : False,
                        'cancel': False
                        }
-    
+     
     icon = 'question'
-    
+     
 #     print(root_msg_box(type_num, title, msg, output_define_d))
     print(msg_box__YES_NO(title, msg, icon ))
 #     print(msg_box__WARNING_MSG_ICON(title, msg, output_define_d))
-    
-    
-    
-    
-    
+     
+     
+     
+     
+     
